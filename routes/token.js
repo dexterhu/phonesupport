bc2d6149971f5dadc3228599040e2413ed1e9ed1
Dexter Hu <hyhu84@gmail.com>
2016-03-22 18:14:39 -0700

    update log

--- routes/token.js
+++ routes/token.js
@@ -3,15 +3,19 @@ var router = express.Router();
 var twilio = require('twilio');
 
 // GET /token/generate
-router.post('/generate', function (req, res) {
+router.post('/generate', function(req, res) {
   var page = req.body.page;
 
   var capability = new twilio.Capability(
-      process.env.TWILIO_ACCOUNT_SID,
-      process.env.TWILIO_AUTH_TOKEN
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
   );
   capability.allowClientOutgoing(process.env.TWILIO_APP_SID);
-  capability.allowClientIncoming(page == "/dashboard"? "support_agent" : "customer");
+  console.log(process.env.TWILIO_ACCOUNT_SID);
+  console.log(process.env.TWILIO_AUTH_TOKEN);
+  console.log(process.env.TWILIO_APP_SID);
+
+  capability.allowClientIncoming(page == "/dashboard" ? "support_agent" : "customer");
 
   var token = capability.generate();
   res.setHeader('Content-Type', 'application/json');
