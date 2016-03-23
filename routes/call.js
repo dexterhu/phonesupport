7b4309586998e5bdb06202fcf89b56196c8c0ea5
Dexter Hu <hyhu84@gmail.com>
2016-03-22 18:33:32 -0700

    update request

--- routes/call.js
+++ routes/call.js
@@ -3,23 +3,24 @@ var router = express.Router();
 var twilio = require('twilio');
 
 // POST /calls/connect
-router.post('/connect', twilio.webhook({validate: false}), function(req, res, next) {
+router.post('/connect', twilio.webhook({ validate: false }), function(req, res, next) {
+  console.log(req.body);
   var phoneNumber = req.body.phoneNumber;
   var callerId = process.env.TWILIO_PHONE_NUMBER;
   var twiml = new twilio.TwimlResponse();
 
   var numberDialer = function(dial) {
-      dial.number(phoneNumber);
+    dial.number(phoneNumber);
   };
 
   var clientDialer = function(dial) {
-      dial.client("support_agent");
+    dial.client("support_agent");
   };
 
   if (phoneNumber != null) {
-    twiml.dial({callerId : callerId}, numberDialer);
-  }else {
-    twiml.dial({callerId : callerId}, clientDialer);
+    twiml.dial({ callerId: callerId }, numberDialer);
+  } else {
+    twiml.dial({ callerId: callerId }, clientDialer);
   }
 
   res.send(twiml.toString());
